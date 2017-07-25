bf115db0bb2cb0dc825b2936647004e72deb9be6
付广辉 <frowhy@gmail.com>
2017-07-25 15:35:15 +0800

    First Commit.

---
+++ src/PrpCrypt.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: frowhy
+ * Date: 2017/7/25
+ * Time: 下午3:31
+ */
+
+namespace Leto\MiniProgramAES;
+
+
+class PrpCrypt
+{
+    public $key;
+
+    function __construct($k)
+    {
+        $this->key = $k;
+    }
+
+    /**
+     * 对密文进行解密
+     *
+     * @param string $aesCipher 需要解密的密文
+     * @param string $aesIV     解密的初始向量
+     *
+     * @return array
+     */
+    public function decrypt($aesCipher, $aesIV)
+    {
+
+        try {
+            $decrypted = base64_encode(openssl_decrypt($aesCipher, "AES-128-CBC", $this->key, OPENSSL_RAW_DATA, $aesIV));
+        } catch (Exception $e) {
+            return [ErrorCode::$IllegalBuffer, null];
+        }
+
+        try {
+            //去除补位字符
+            $pkc_encoder = new PKCS7Encoder;
+            $result      = $pkc_encoder->decode($decrypted);
+
+        } catch (Exception $e) {
+            //print $e;
+            return [ErrorCode::$IllegalBuffer, null];
+        }
+
+        return [0, $result];
+    }
+}
---
+++ src/WXBizDataCrypt.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: frowhy
+ * Date: 2017/7/25
+ * Time: 下午3:31
+ */
+
+namespace Leto\MiniProgramAES;
+
+/**
+ * 对微信小程序用户加密数据的解密示例代码.
+ *
+ * @copyright Copyright (c) 1998-2014 Tencent Inc.
+ */
+
+
+class WXBizDataCrypt
+{
+    private $appid;
+    private $sessionKey;
+
+    /**
+     * 构造函数
+     *
+     * @param $sessionKey string 用户在小程序登录后获取的会话密钥
+     * @param $appid      string 小程序的appid
+     *
+     * @return WXBizDataCrypt
+     */
+    public function __construct($appid, $sessionKey)
+    {
+        $this->sessionKey = $sessionKey;
+        $this->appid      = $appid;
+
+        return $this;
+    }
+
+
+    /**
+     * 检验数据的真实性，并且获取解密后的明文.
+     *
+     * @param $encryptedData string 加密的用户数据
+     * @param $iv            string 与用户数据一同返回的初始向量
+     * @param $data          string 解密后的原文
+     *
+     * @return int 成功0，失败返回对应的错误码
+     */
+    public function decryptData($encryptedData, $iv, &$data)
+    {
+        if (strlen($this->sessionKey) != 24) {
+            return ErrorCode::$IllegalAesKey;
+        }
+        $aesKey = base64_decode($this->sessionKey);
+
+
+        if (strlen($iv) != 24) {
+            return ErrorCode::$IllegalIv;
+        }
+        $aesIV = base64_decode($iv);
+
+        $aesCipher = base64_decode($encryptedData);
+
+        $pc     = new PrpCrypt($aesKey);
+        $result = $pc->decrypt($aesCipher, $aesIV);
+
+        if ($result[0] != 0) {
+            return $result[0];
+        }
+
+        $dataObj = json_decode($result[1]);
+        if ($dataObj == null) {
+            return ErrorCode::$IllegalBuffer;
+        }
+        if ($dataObj->watermark->appid != $this->appid) {
+            return ErrorCode::$IllegalBuffer;
+        }
+        $data = $result[1];
+
+        return ErrorCode::$OK;
+    }
+
+}
